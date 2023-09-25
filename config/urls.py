22daa0017337f4a4012743a5a80468eb4feabc79
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-09-25 15:22:22 -0400

    update

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sistema/', include('apps.inicio.urls'))
+    path('sistema/', include('apps.inicio.urls')),
+    path('auth/login/', include('apps.usuarios.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
