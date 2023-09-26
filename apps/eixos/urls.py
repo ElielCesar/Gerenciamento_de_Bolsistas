351cbe181ae46a55484d2c0137e84343c40f9ed4
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-09-26 17:50:37 -0400

    update CRUD

--- apps/eixos/urls.py
+++ apps/eixos/urls.py
@@ -3,5 +3,8 @@ from apps.eixos import views
 
 urlpatterns = [
     path('cadastrar_eixo/', views.cadastrar_eixo, name='cadastrar_eixo'),
-    path('listar_eixos/', views.listar_eixos, name='listar_eixos')
+    path('listar_eixos/', views.listar_eixos, name='listar_eixos'),
+    path('editar_eixo/', views.editar_eixo, name='editar_eixo'),
+    path('detalhes/<int:id_eixo>/', views.detalhes_eixo, name='detalhes_eixo'),
+    path('deletar_eixo/<int:id_eixo>/', views.deletar_eixo, name='deletar_eixo')
 ]
--- apps/eixos/views.py
+++ apps/eixos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from apps.eixos.forms import Cadastrar_Eixo
 from apps.eixos.models import Eixos
 from django.contrib import messages
@@ -16,11 +17,39 @@ def cadastrar_eixo(request):
 
     return render(request, 'eixos/cadastrar.html', {'form':form})
 
+
 def listar_eixos(request):
     if request.method == 'GET':
         eixos = Eixos.objects.all().values()
         return render(request, 'eixos/listar.html', {'eixos':eixos})
 
-def editar_eixos(request, id_eixo):
-    pass
+
+def editar_eixo(request):
+    if request.method == 'GET':
+        eixos = Eixos.objects.all().values()
+        return render(request, 'eixos/editar.html', {'eixos':eixos})
+
+
+def detalhes_eixo(request, id_eixo):
+
+    eixo = get_object_or_404(Eixos, pk=id_eixo)
+    
+    if request.method == 'POST':
+        form = Cadastrar_Eixo(request.POST, instance=eixo)
+        if form.is_valid():
+            form.save()
+            return redirect('editar_eixo')
+    else:
+        form = Cadastrar_Eixo(instance=eixo)
+
+    return render(request, 'eixos/update.html', {'form': form, 'eixo':eixo})
+
+def deletar_eixo(request, id_eixo):
+    eixo = get_object_or_404(Eixos, pk=id_eixo)
+
+    if request.method == 'POST':
+        eixo.delete()
+        return redirect('editar_eixo')
+    else:
+        return render(request, 'eixos/confirmar.html', {'eixo':eixo})
 
