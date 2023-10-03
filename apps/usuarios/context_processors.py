ad836f4007b7a7b42fd63f26949d0cb0a14623b7
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-10-03 18:59:50 -0400

    update
    nada importante foi alterado nesse commit.

--- apps/usuarios/context_processors.py
+++ apps/usuarios/context_processors.py
@@ -6,5 +6,4 @@ def menu_context(request):
     is_bolsista = request.user.groups.filter(name='Bolsistas').exists()
     is_financeiro = request.user.groups.filter(name='Financeiro').exists()
 
-
     return {'is_coordenador': is_coordenador, 'is_bolsista': is_bolsista, 'is_financeiro': is_financeiro }
